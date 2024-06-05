35b630ef5aea948cf48a81314ce990104c1a502d
m.kolo <mykola.kolomoyets@phenomenon-studio.com>
2024-06-05 17:07:24 +0300

    feat(animations): added animations to coin introduction section

--- src/modules/Home/components/CoinIntroductionSection/CoinIntroductionSection.tsx
+++ src/modules/Home/components/CoinIntroductionSection/CoinIntroductionSection.tsx
@@ -5,6 +5,7 @@ import Typography from '@/ui/Typography';
 import Container from '@/ui/Container';
 import CoinAdvantageCard from './components/CoinAdvantageCard';
 import s from './CoinIntroductionSection.module.css';
+import { motion } from 'framer-motion';
 
 const CoinIntroductionSection: React.FC = () => {
     return (
@@ -12,26 +13,96 @@ const CoinIntroductionSection: React.FC = () => {
             <section>
                 <header className={s.header}>
                     <Typography className={s.title} variant="title-h2" asChild>
-                        <h1>Introducing GPSC:The Stablecoin That Powers Our Ecosystem</h1>
+                        <motion.h1
+                            initial={{
+                                opacity: 0,
+                                y: '25%',
+                                scaleY: 0.95,
+                            }}
+                            whileInView={{
+                                opacity: 1,
+                                y: 0,
+                                scaleY: 1,
+                            }}
+                            transition={{
+                                duration: 0.75,
+                                ease: 'easeInOut',
+                                delay: 0.1,
+                            }}
+                            viewport={{
+                                amount: 0.9,
+                            }}
+                        >
+                            Introducing GPSC:The Stablecoin That Powers Our Ecosystem
+                        </motion.h1>
                     </Typography>
                     <div className={s['subtitle-wrap']}>
                         <Typography className={s.subtitle} variant="text-md" asChild>
-                            <h3>
-                                <a className={clsx(s.link, 'focus-primary')} href="#">
+                            <motion.h3>
+                                <motion.a
+                                    className={clsx(s.link, 'focus-primary')}
+                                    href="#"
+                                    initial={{
+                                        opacity: 0,
+                                        y: '-10px',
+                                    }}
+                                    whileInView={{
+                                        opacity: 1,
+                                        y: 0,
+                                    }}
+                                    transition={{
+                                        duration: 0.75,
+                                        ease: 'easeInOut',
+                                        delay: 0.8,
+                                    }}
+                                >
                                     GlobalX (GPSC)
-                                </a>
-                                &nbsp;is a cryptocurrency token that has emerged as a key player in the ever-expanding
-                                digital asset landscape.
-                            </h3>
+                                </motion.a>
+                                &nbsp;
+                                <motion.span
+                                    initial={{
+                                        opacity: 0,
+                                        y: '-10px',
+                                    }}
+                                    whileInView={{
+                                        opacity: 1,
+                                        y: 0,
+                                    }}
+                                    transition={{
+                                        duration: 0.75,
+                                        ease: 'easeInOut',
+                                        delay: 1.25,
+                                    }}
+                                >
+                                    is a cryptocurrency token that has emerged as a key player in the ever-expanding
+                                    digital asset landscape.
+                                </motion.span>
+                            </motion.h3>
                         </Typography>
                     </div>
                 </header>
                 <ul className={s.advantages}>
-                    {COIN_ADVANTAGES.map((advantage) => {
+                    {COIN_ADVANTAGES.map((advantage, index) => {
                         return (
-                            <li key={advantage.alt} className={s.advantage}>
+                            <motion.li
+                                key={advantage.alt}
+                                className={s.advantage}
+                                initial={{
+                                    opacity: 0,
+                                    y: '40px',
+                                }}
+                                whileInView={{
+                                    opacity: 1,
+                                    y: 0,
+                                }}
+                                transition={{
+                                    duration: 0.5,
+                                    ease: 'easeInOut',
+                                    delay: 0.75 + 0.2 * (index + 1),
+                                }}
+                            >
                                 <CoinAdvantageCard {...advantage} />
-                            </li>
+                            </motion.li>
                         );
                     })}
                 </ul>
